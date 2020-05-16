bbb80509c69b3693d408d41bb4e04c61eba64fd4
Mohak Chugh <me.mohakchugh@gmail.com>
2020-05-16 16:00:56 +0530

    basic login integrated

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as axios from 'axios';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  email: string;
+  password: string;
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  login() {
+    axios.default.post('http://localhost:3000/login', {
+      email: this.email,
+      password: this.password
+    })
+      .then(response => {
+        console.log(response.data);
+        if (response.data.error === false) {
+          if (this.email !== 'admin@admin.com') {
+            this.router.navigateByUrl('plantmainpage');
+          } else {
+            localStorage.setItem('isAdmin', 'true');
+          }
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 }
