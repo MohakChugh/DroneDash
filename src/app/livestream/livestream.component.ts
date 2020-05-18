a7545a5d38670a2dae639cdeb34a01229b152cc7
Mohak Chugh <me.mohakchugh@gmail.com>
2020-05-18 14:37:27 +0530

    plant name saved in localstorage

--- src/app/livestream/livestream.component.ts
+++ src/app/livestream/livestream.component.ts
@@ -8,11 +8,14 @@ import { LogoutService } from '../logout.service';
 })
 export class LivestreamComponent implements OnInit {
 
-  CompanyName = 'Hindalco';
+  CompanyName: string;
   CurrentDateandTime = new Date();
+  livestreamBaseUrl = 'ws://35.188.41.41:8000/live/';
   constructor(public logout: LogoutService) { }
 
   ngOnInit(): void {
+    this.CompanyName = localStorage.getItem('plant');
+    this.livestreamBaseUrl = this.livestreamBaseUrl + this.CompanyName + '.flv';
+    console.log(this.livestreamBaseUrl);
   }
-
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -30,11 +30,20 @@ export class LoginComponent implements OnInit {
         if (response.data.error === false && !!response.data.token === true) {
           // Checking if the user is Admin
           if (this.email !== 'admin@admin.com') {
+            // If the user is not admin
             localStorage.setItem('isAuth', 'true');
+
+            // Save the name of the plant in the local storage
+            localStorage.setItem('plant', this.email);
+
+            // Route the user to plant dashboard
             this.router.navigateByUrl('plantmainpage');
           } else {
+            // If the user is Admin
             localStorage.setItem('isAuth', 'true');
             localStorage.setItem('isAdmin', 'true');
+
+            // Route the admin to admin dashboard
             this.router.navigateByUrl('admindashboard');
           }
         } else {
