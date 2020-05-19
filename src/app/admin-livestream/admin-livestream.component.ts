c610b25544b91477f23ad8043fd88c5fd638486b
Mohak Chugh <me.mohakchugh@gmail.com>
2020-05-19 17:33:18 +0530

    admin livestream added

--- src/app/admin-livestream/admin-livestream.component.ts
+++ src/app/admin-livestream/admin-livestream.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LogoutService } from '../logout.service';
+import * as axios from 'axios';
+import * as flvjs from 'flv.js';
 
 @Component({
   selector: 'app-admin-livestream',
@@ -11,10 +13,47 @@ export class AdminLivestreamComponent implements OnInit {
   // tslint:disable-next-line: variable-name
   company_name = 'Hindalco';
   Time = new Date();
+  names = [];
+  liveStreamBaseUrl = 'ws://35.222.157.43:8000/live/';
+  urls = [];
+  liveStreamOn = false;
+  liveStreamUrl: string;
   constructor(public logout: LogoutService) { }
 
   ngOnInit(): void {
+    axios.default.get('https://omnipresent-dashboard-backend.herokuapp.com/getNames')
+      .then(response => {
+        if (response.data.error === false) {
+          this.names = response.data.response;
+          // tslint:disable-next-line: prefer-for-of
+          for (let i = 0; i < this.names.length; i++) {
+            this.urls.push(`${this.liveStreamBaseUrl}${this.names[i]}`);
+          }
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        alert('An Error Occured while getting the livestreams! Please check the Console for details');
+      });
   }
 
-
+  async player(index) {
+    this.liveStreamUrl = this.urls[index];
+    this.liveStreamOn = true;
+    if (flvjs.default.isSupported()) {
+      const videoElement = document.getElementById('videoElement') as HTMLMediaElement;
+      const flvPlayer = flvjs.default.createPlayer({
+        type: 'flv',
+        url: this.liveStreamUrl
+      });
+      flvPlayer.attachMediaElement(videoElement);
+      flvPlayer.load();
+      try {
+        await flvPlayer.play();
+      } catch (err) {
+        console.log(err);
+        alert('Error Occured! Stream is not working!');
+      }
+    }
+  }
 }
