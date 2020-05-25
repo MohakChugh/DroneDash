7f633d750d9251d9d516a1a7e6805d2a03fd631f
Mohak Chugh <me.mohakchugh@gmail.com>
2020-05-25 13:40:57 +0530

    file upload and url added in database

--- src/app/report-generation/report-generation.component.ts
+++ src/app/report-generation/report-generation.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import * as axios from 'axios';
+import { GraphQLClient } from 'graphql-request';
 
 @Component({
   selector: 'app-report-generation',
@@ -37,12 +38,41 @@ export class ReportGenerationComponent implements OnInit {
       const formdata = new FormData();
       formdata.append('file', this.file);
       this.http.post(this.fileUploadUrl, formdata)
-        .subscribe(res => {
+        .subscribe(async res => {
           this.response = res;
           if (this.response.error === false) {
             this.location = this.response.Data[0].Location;
             // Now add this location in the url of the file being uploaded
             // Add the data from here into the database, with an access level of primary
+            const client = new GraphQLClient('https://rbacksystem-fileupload.herokuapp.com/v1/graphql', {
+              headers: {
+                'content-type': 'application/json',
+                'x-hasura-admin-secret': 'omnipresent'
+              },
+            });
+
+            const query = `mutation MyMutation {
+              insert_rback(objects: {
+                access: "AdminOnly",
+                dateOfReportWriting: "${this.dateOfReport}",
+                documentReferenceNumber: "${this.documentReferenceNumber}",
+                plantName: "${this.plantName}",
+                reportMonth: "${this.reportMonth}",
+                reportName: "${this.reportName}",
+                reportby: "${this.reportBy}",
+                fileUrl: "${this.location}"
+              }) {
+                returning {
+                  id
+                }
+              }
+            }`;
+
+            await client.request(query)
+              .then(data => {
+                alert('Uploaded!');
+              })
+              .catch((err) => err);
           }
         });
     } else {
