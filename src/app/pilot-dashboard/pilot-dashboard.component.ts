3b27393a0edc683b257dce8c49e57d55c26ddd05
Mohak Chugh <me.mohakchugh@gmail.com>
2020-05-30 15:58:31 +0530

    task and todo lists working properly

--- src/app/pilot-dashboard/pilot-dashboard.component.ts
+++ src/app/pilot-dashboard/pilot-dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataStoreService } from '../data-store.service';
 import { LogoutService } from '../logout.service';
+import { GraphQLClient } from 'graphql-request';
 
 @Component({
   selector: 'app-pilot-dashboard',
@@ -11,10 +12,31 @@ export class PilotDashboardComponent implements OnInit {
 
   pilotName = '';
   isSent = false;
+  res: any;
+  todoList = [];
   constructor(private dataStore: DataStoreService, public logout: LogoutService) { }
 
-  ngOnInit(): void {
+  async ngOnInit() {
     this.pilotName = this.dataStore.getDataStore('plant');
+    const client = new GraphQLClient('https://rbacksystem-fileupload.herokuapp.com/v1/graphql', {
+      headers: {
+        'content-type': 'application/json',
+        'x-hasura-admin-secret': 'omnipresent'
+      },
+    });
+    const query = `query MyQuery {
+      todoList(where: {plant: {_eq: "${this.pilotName}"}}) {
+        plant
+        text
+        id
+      }
+    }`;
+    await client.request(query)
+      .then(data => {
+        this.res = data;
+        this.todoList = this.res.todoList;
+      })
+      .catch(err => console.log(err));
   }
 
 }
