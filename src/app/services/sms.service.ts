a0f4cbeced7f26b5551ed5d79729338145e2e64c
Mohak Chugh <me.mohakchugh@gmail.com>
2020-06-18 20:05:53 +0530

    sms service changed

--- src/app/services/sms.service.ts
+++ src/app/services/sms.service.ts
@@ -1,23 +1,33 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import * as axios from 'axios';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SmsService {
 
-  smsUrl = 'https://2factor.in/API/V1/6dd0d498-b156-11ea-9fa5-0200cd936042/ADDON_SERVICES/SEND/TSMS';
-  constructor(private http: HttpClient) { }
+  // smsUrl = 'https://2factor.in/API/V1/6dd0d498-b156-11ea-9fa5-0200cd936042/ADDON_SERVICES/SEND/TSMS';
+  smsUrl = 'https://2factor.in/API/R1/?module=PROMO_SMS&apikey=6dd0d498-b156-11ea-9fa5-0200cd936042&to=9810178257&from=omnipr&msg=HI';
+  constructor() { }
 
   sendsms(message: string, phonenumber: number) {
     const form = new FormData();
-    form.append('From', 'omnipr');
+    form.append('From', 'Omnipr');
     form.append('TemplateName', 'notification');
     form.append('To', `${phonenumber}`);
-
-    this.http.post(this.smsUrl, form)
-    .subscribe(res => {
-      console.log(res);
-    });
+    axios.default.post(this.smsUrl, {
+      From: 'omnipr',
+      TemplateName: `notification`,
+      To: `${phonenumber}`
+    }).then(
+      (response) => {
+        console.log('Request sent');
+        console.log(response.data);
+      }
+    ).catch(
+      () => {
+        console.log('Error in request');
+      }
+    );
   }
 }
